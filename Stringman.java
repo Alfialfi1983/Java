566d694218c8e43dabe403c86f4d42d0115a5a4d
Alfialfi1983 <117815598+Alfialfi1983@users.noreply.github.com>
2023-04-03 14:11:45 +0530

    Add files via upload

--- Stringman.java
+++ Stringman.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
-public class Stringman{
-    public static void main(String[] args) {
-        System.out.println("Enter The String");
-        Scanner sc = new Scanner(System.in);
-        String str1 = sc.nextLine();
-        System.out.println("Length of String = "+str1.length());
-        System.out.println("Character at First position  = "+str1.charAt(1));
-        System.out.println("String Contains 'Col' sequence :"+str1.contains("Col"));
-        System.out.println("String ends with e : "+str1.endsWith("e"));
-        System.out.println("Replace'col' with 'kol' : "+str1.replaceAll("Col","kol"));
-        System.out.println("LOWERCASE : "+str1.toLowerCase());
-        System.out.println("UPPERCASE : "+str1.toUpperCase());
+public class Stringman
+{
+public static void main(String[] args)
+{
+System.out.println("Enter The String");
+Scanner sc = new Scanner(System.in);
+StringBuffer sb=new StringBuffer("Hello");
+sb.append("everyone");
+String str1 = sc.nextLine();
+System.out.println("Length of String = "+str1.length());
+System.out.println("Character at First position  = "+str1.charAt(0));
+System.out.println("LOWERCASE : "+str1.toLowerCase());
+System.out.println("UPPERCASE : "+str1.toUpperCase());
+System.out.println("append : "+sb);
+System.out.println("replace "+str1.replace("h","@"));
+System.out.println("indexof "+str1.indexOf("e"));
 }
 }
-
