99b8e7909e8ddfafb1f5b1d29f59301079492b48
Alfialfi1983 <117815598+Alfialfi1983@users.noreply.github.com>
2023-04-17 13:54:57 +0530

    Update product.java

--- product.java
+++ product.java
@@ -13,20 +13,20 @@ class product
 
     public static void main(String[] args)
       {
-        product p1 = new product("101","Shampoo",100);
-        product p2 = new product("102","Lotion",128);
-        product p3 = new product("103","Shower Gel",790);
+        product p1 = new product("A","TV",10000);
+        product p2 = new product("B","mobile",8000);
+        product p3 = new product("C","Clock",5000);
         if(p1.price<p2.price && p1.price<p3.price)
           {
-	    System.out.println("\nLowest price is "+p1.price);
+	    System.out.println("\nLowest price is "+p1.pname);
 	  }
 	if(p2.price<p1.price && p2.price<p3.price)
           {
-	     System.out.println("\nLowest price is "+p2.price);
+	     System.out.println("\nLowest price is "+p2.pname);
 	  }
 	if(p3.price<p1.price && p3.price<p2.price)
           {
-	     System.out.println("\nLowest price is "+p3.price);
+	     System.out.println("\nLowest price is "+p3.pname);
 	  }
  
       }
